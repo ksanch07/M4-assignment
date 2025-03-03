ed73fa589ec3fafa5015c98cedba65c4706f991e
Keoni Sanchez <keoni.sanchez@gmail.com>
2025-03-02 19:27:19 -0800

    coinflip test

---
+++ js/01-coinflip-redux.js
@@ -0,0 +1,14 @@
+let coinFlip
+let numFlips = parseInt(prompt("Enter a number of coin flips"))
+
+if (isNaN(numFlips)|| numFlips < 0) {
+    alert("Please enter a valid number greater than 0")
+}
+for (let i = 0; i < numFlips; i++) {
+    coinFlip = Math.round(Math.random())
+    if (coinFlip) {
+        console.log("Heads")
+    } else {
+        console.log("Tails")
+    }
+}
